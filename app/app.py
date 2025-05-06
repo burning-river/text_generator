6ef5e7a6727d656dd6b81e05193508f6f1d33e94
Saurabh Kumar <saurabhkumar3400@gmail.com>
2025-05-06 14:40:34 -0400

    outputs input text

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ import os
 from huggingface_hub import HfFolder, Repository
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 from flask import Flask, request, jsonify, render_template
+import re
 
 hf_token = '**'
 repo_name = "**"
@@ -21,12 +22,18 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    test_text = [text for text in request.form.values()]
-    input_ids = dwnld_tokenizer(test_text[0], return_tensors="pt").input_ids
-    output_tokens = dwnld_model.generate(input_ids, max_length=50, num_return_sequences=1)
-    output = dwnld_tokenizer.decode(output_tokens[0], skip_special_tokens=True)
 
-    return render_template('index.html', prediction_text=f'Summarized text: {output}')
+    test_text = [text for text in request.form.values()]
+    sentences = re.split('(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|\!)\s', test_text[0])
+    generated_text = ''
+    for sentence in sentences:
+        delimiter = sentence[-1]
+        input_ids = dwnld_tokenizer(sentence[:-1], return_tensors="pt").input_ids
+        output_tokens = dwnld_model.generate(input_ids, max_length=50, num_return_sequences=1)
+        output = dwnld_tokenizer.decode(output_tokens[0], skip_special_tokens=True)
+        generated_text += output + delimiter + ' '
+
+    return render_template('index.html', input_text = test_text[0], prediction_text=generated_text)
 
 
 if __name__ == "__main__":
